87d31fbd6c050f03d83fc88472b77c091b3984f4
kosakosinski <kosakosinski96@gmail.com>
2021-09-12 19:24:23 +0000

    Change version

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello World!\n");
-	printf("Version %d.%d\n", hello_VERSION_MAJOR, hello_VERSION_MINOR);
+	printf("Version %d.%d.%d\n", hello_VERSION_MAJOR, hello_VERSION_MINOR, hello_VERSION_PATCH);
 	return 0;
 }
